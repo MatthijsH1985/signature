b3a245c9747df0dd0483e42f33bf8036d83ea2d1
hondebrink <matthijs.hondebrink@topicus.nl>
2024-08-28 08:55:51 +0200

    Wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,7 +37,6 @@ export class AppComponent {
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Handtekening van Befo</title>
         </head>
         <body>
             ${htmlContent}
